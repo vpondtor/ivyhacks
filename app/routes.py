bde9227487df67fa9d6b0ed62887285e07b9e1ce
Virak Pond-Tor <virakpond-tor@Viraks-MacBook-Pro.local>
2020-10-03 14:33:06 -0400

    made the web app

---
+++ app/routes.py
@@ -0,0 +1,24 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    proj = {
+        'name': 'School Connect'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', 
+        title = 'Home', 
+        user = user, 
+        posts = posts,
+        proj = proj)
